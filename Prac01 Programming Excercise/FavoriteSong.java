f1c177bdca18a844fd5c83476f36ebde6fc69c9b
Felixsie <31508904+Felixsie@users.noreply.github.com>
2018-07-10 04:50:14 -0700

    Finished Prac01 Programming Exercise

---
+++ Prac01 Programming Excercise/FavoriteSong.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Lenovo on 7/10/2018.
+ */
+public class FavoriteSong
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Shout to the Lord all the Earth, let us sing");
+        System.out.println("Power and majesty, praise to the King");
+        System.out.println("Mountains bow down and the seas will roar");
+        System.out.println("At the sound of Your name");
+    }
+}
---
+++ Prac01 Programming Excercise/MovieQuote.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Lenovo on 7/10/2018.
+ */
+public class MovieQuote
+{
+    public static void main(String[] args)
+    {
+        System.out.println("It's like the number of zero. It's empty, but at the same time holds infinite possibilities.");
+    }
+}
